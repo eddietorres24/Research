e981339d0ec689debcb288698d7d1a0c138accac
eddietorres24 <89259715+eddietorres24@users.noreply.github.com>
2022-07-17 16:28:03 -0400

    Create MapCutAndRun.sh

---
+++ MapCutAndRun.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#SBATCH --job-name=zl_mapChIPseq
+#SBATCH --partition=batch
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=zlewis@uga.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=50gb
+#SBATCH --time=08:00:00
+#SBATCH --output=../MapCutAndRun.%j.out
+#SBATCH --error=../MapCutAndRun.%j.err
+
+cd $SLURM_SUBMIT_DIR
+
+#read in variables from the config file ($threads, $FASTQ, $OUTDIR, )
+
+source config.txt
+
+# #process reads using trimGalore
+#
+ ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+ trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+#
+FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz" #Don't forget the *
+#
+ mkdir "${OUTDIR}/SortedBamFiles"
+ mkdir "${OUTDIR}/BigWigs"
+ mkdir "${OUTDIR}/Peaks"
+#mkdir "$OUTDIR/HomerTagDirectories"
+#mkdir "$OUTDIR/TdfFiles"
+#
+#Iterate over the files
+for f in $FILES
+do
+#
+# 	#Examples to Get Different parts of the file name
+# 		#See here for details: http://tldp.org/LDP/abs/html/refcards.html#AEN22664
+		#${string//substring/replacement}
+# 		#dir=${f%/*}
+		
+	file=${f##*/}
+	#remove ending from file name to create shorter names for bam files and other downstream output
+	name=${file/%_S[1-12]*_L001_R1_001_val_1.fq.gz/}
+
+#
+# 	# File Vars
+# 	#use sed to get the name of the second read matching the input file
+	read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
+	#variable for naming bam file
+ 	bam="${OUTDIR}/SortedBamFiles/${name}.bam"
+	#variable name for bigwig output
+	bigwig="${OUTDIR}/BigWigs/${name}"
+	#QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
+#
+
+ml SAMtools/1.9-GCC-8.3.0
+ml BWA/0.7.17-GCC-8.3.0
+#
+bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+samtools index "$bam"
+
+#samtools view -b -q 30 $bam > "$QualityBam"
+#samtools index "$QualityBam"
+
+############################
+# # #deeptools
+
+ml deepTools/3.3.1-intel-2019b-Python-3.7.4
+# #use these parameters for ChIP data
+bamCoverage -p $THREADS $MNase -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}${MN}.bw"
+
+done
