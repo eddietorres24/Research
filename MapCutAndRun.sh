df1caa0cce0d6fafc2c6a6e5e1b84375869116d7
eddietorres24 <89259715+eddietorres24@users.noreply.github.com>
2023-04-21 14:57:26 -0400

    Update MapCutAndRun.sh

--- MapCutAndRun.sh
+++ MapCutAndRun.sh
@@ -29,7 +29,7 @@ fi
 
 # #process reads using trimGalore
 #
- ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+ ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4 BWA/0.7.17-GCC-8.3.0
  trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
 #
 FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz" #Don't forget the *
@@ -39,7 +39,8 @@ FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz" #Don't forget the *
  mkdir "${OUTDIR}/Peaks"
 #mkdir "$OUTDIR/HomerTagDirectories"
 #mkdir "$OUTDIR/TdfFiles"
-#
+bwa index -p $GENOME
+
 #Iterate over the files
 for f in $FILES
 do
@@ -65,9 +66,7 @@ do
 #
 
 ml SAMtools/1.9-GCC-8.3.0
-ml BWA/0.7.17-GCC-8.3.0
 #
-bwa index $GENOME
 bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
 samtools index "$bam"
 
