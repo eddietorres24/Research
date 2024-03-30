e99ce0e81ab8b2cee66f6eb26458eba7e132e48d
eddietorres24 <89259715+eddietorres24@users.noreply.github.com>
2024-03-30 13:53:52 -0400

    Update MapRNAseq.sh

--- MapRNAseq.sh
+++ MapRNAseq.sh
@@ -52,7 +52,7 @@ bwDir= mkdir -p "${outdir}/bigWig/${accession}"
 #input file variables
   read1=${fastqPath}/${accession}/${accession}_1.fastq.gz
   read2=${fastqPath}/${accession}/${accession}_2.fastq.gz
-  unpaired=${fastqPath}/${accession}/${accession}.fastq.gz
+  #unpaired=${fastqPath}/${accession}/${accession}.fastq.gz
 
 # #make output file folders
 # trimmed= mkdir "${outdir}/TrimmedFastQs/${accession}"
