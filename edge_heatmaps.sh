875c0d4a940bc0cc355253faf047e5d42bb03267
eddietorres24 <89259715+eddietorres24@users.noreply.github.com>
2024-05-16 09:30:14 -0400

    Update edge_heatmaps.sh

--- edge_heatmaps.sh
+++ edge_heatmaps.sh
@@ -10,23 +10,29 @@
 #SBATCH --output=../HeatMap.%j.out
 #SBATCH --error=../HeatMap.%j.err
 
-computeMatrix reference-point -p 12 -R /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/Peaks/K9_Peaks.txt -S \
-      /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/BigWigs/119-49_CUT_RUN_WT_H3K9__S37_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
-      /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/BigWigs/133-75_CUTnRUN_WT_H3K27m2m3__S72_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
-      /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/BigWigs/119-41_CUT_RUN_WT_K27me3_.bin_25.smooth_75Bulk.bw \
-      /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/BigWigs/119-50_CUT_RUN_hda-1_H3K9__S38_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
-      /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/BigWigs/6147_136-18_CUTnRUN_HDA-1_H3K27me2m3.bin_25.smooth_75Bulk.bw \
-      /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/BigWigs/119-42_CUT_RUN_hda-1_K27me3__S30_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
+OUTDIR2="/scratch/evt82290/Run136/BigWigs"
+
+computeMatrix reference-point -p 12 -R Figure2_K27regions_Scaledcenter_FileToCheckOrderFINAL_ZL.txt \
+ -S  $OUTDIR2/6147_136-1_ChIP_WT_H3K27me3_abcam_Rep2.bin_25.smooth_75Bulk.bw \
+     $OUTDIR2/6147_136-2_ChIP_cac-1_H3K27me3_abcam_Rep2.bin_25.smooth_75Bulk.bw \
+     $OUTDIR2/6147_136-3_ChIP_cac-2_H3K27me3_abcam_Rep2_S3_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
+     $OUTDIR2/6147_136-4_ChIP_cac-3_H3K27me3_abcam_Rep2_S4_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
+     $OUTDIR2/6147_136-83_ChIP_set-7_H3K27me3_CS_Rep2_S82_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
       --referencePoint TSS \
       -b 3000 -a 3000 \
-      -o /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/MatrixFiles/Figure2_K9regions_FivePrime.matrix \
-      --outFileNameMatrix Figure2_K9regions_FivePrime.matrix.txt \
+      -o edge_matrix_PRC2.gz \
       --sortRegions keep \
-   --missingDataAsZero -bs 10
+      --missingDataAsZero \
+      -bs 10
  # --sortUsing sum --sortUsingSamples 5 2
       #--sortRegions descend --sortUsing sum \
        #  --sortUsingSamples 3
 
 
 #https://deeptools.readthedocs.io/en/develop/content/tools/plotHeatmap.html
-plotHeatmap -m /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/MatrixFiles/test.matrix -o /scratch/zlewis/HDA1_MS/Figure2/Figure2_Output/HeatmapFiles/Figure2_K9peaks_FivePrime.png --sortRegions keep --refPointLabel "5' edge" --samplesLabel "WT K9" "WT K27me2/3" "WT K27me3" "hda-1 K9" "hda-1 K27me2/3"  "hda-1 K27me3" --zMax 10 10 12 10 10 12 --colorMap 'Blues' 'Greens' 'Greens' 'Blues' 'Greens' 'Greens'
+plotHeatmap -m edge_matrix_PRC2.gz \
+  --sortRegions keep \
+  --refPointLabel "5' edge" \
+  --samplesLabel "WT K27me3" "cac-1 K27me3" "cac-2 K27me3" "cac-3 K27me3"  "set-7 K27me3" \
+  --zMax 20 20 20 20 20 \
+  --colorMap 'Blues'
