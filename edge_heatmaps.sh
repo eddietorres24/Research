7e2b9e35443015b3bfa07b6c6df98667f385cf57
eddietorres24 <89259715+eddietorres24@users.noreply.github.com>
2024-05-16 11:38:42 -0400

    Update edge_heatmaps.sh

--- edge_heatmaps.sh
+++ edge_heatmaps.sh
@@ -20,11 +20,7 @@ computeMatrix reference-point --referencePoint TSS -p 12 -b 3000 -a 3000 \
      $OUTDIR2/6147_136-3_ChIP_cac-2_H3K27me3_abcam_Rep2_S3_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
      $OUTDIR2/6147_136-4_ChIP_cac-3_H3K27me3_abcam_Rep2_S4_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
      $OUTDIR2/6147_136-83_ChIP_set-7_H3K27me3_CS_Rep2_S82_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw \
-     -R Figure2_K27regions_Scaledcenter_FileToCheckOrderFINAL_ZL.txt \
-     -o /Research/edge_matrix.matrix \
-     --sortRegions keep \
-     --missingDataAsZero \
-     -bs 10
+     -R Figure2_K27regions_Scaledcenter_FileToCheckOrderFINAL_ZL.txt -o edge_matrix.matrix --sortRegions keep --missingDataAsZero -bs 10
 
 
 
@@ -32,7 +28,7 @@ computeMatrix reference-point --referencePoint TSS -p 12 -b 3000 -a 3000 \
 
 
 #https://deeptools.readthedocs.io/en/develop/content/tools/plotHeatmap.html
-plotHeatmap -m /Research/edge_matrix.matrix -out 2024_H3K27me3_CAF_edges.png \
+plotHeatmap -m edge_matrix.matrix -out 2024_H3K27me3_CAF_edges.png \
   --sortRegions keep \
   --refPointLabel "5' edge" \
   --samplesLabel "WT K27me3" "cac-1 K27me3" "cac-2 K27me3" "cac-3 K27me3"  "set-7 K27me3" \
