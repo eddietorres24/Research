325135a3b02c3d1659e24300b4997c26c1e7c7ed
eddietorres24 <eddietorres136@gmail.com>
2025-01-21 15:14:42 -0500

    GATK

---
+++ GATK_ET.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=cac_GATK
+#SBATCH --partition=batch
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=evt82290@uga.edu
+#SBATCH --ntasks=1
+#SBATCH --mem=10gb
+#SBATCH --time=08:00:00
+#SBATCH --output=../GATK/logs/%x.out
+#SBATCH --error=../GATK/logs/%j.err
+
+#change directroy & load GATK
+cd $SLURM_SUBMIT_DIR
+ml GATK/3.8-0-Java-1.8.0_144
+
+#set working & output directories
+OUTDIR="/scratch/evt82290/Run136/GATK"
+BAMDIR="/scratch/evt82290/Run136/SortedBamFiles"
+
+#if output directory doesn't exist, create it
+if [ ! -d $OUTDIR ]
+then
+    mkdir -p $OUTDIR
+fi
+###
+
+#Submit job with the analysis that you want
+java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T HaplotypeCaller \
+-R ~/Research/GCA_000182925.2_NC12_genomic_GFFtoGTFconversion.gtf \
+-I ${BAMDIR}/6147_136-11_ChIP_WT_input.bam \
+-O ${OUTDIR}/WT.vcf.gz\
+
+#6147_136-11_ChIP_WT_input.bam
+#6147_136-12_ChIP_cac-1_input.bam
+#6147_136-13_ChIP_cac-2_input.bam
+#6147_136-14_ChIP_cac-3_input.bam
+#147_136-92_ChIP_set-7_input_S91_L001_R1_001_val_1.fq.gz.bam
