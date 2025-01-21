09412b5934e69ace0ec216f34aab2c1015ab325f
eddietorres24 <eddietorres136@gmail.com>
2025-01-21 15:27:23 -0500

    Update GATK_ET.sh

--- GATK_ET.sh
+++ GATK_ET.sh
@@ -24,7 +24,7 @@ then
 fi
 
 #HaplotypeCaller (-T chooses what tool you want to use)
-java -jar GenomeAnalysisTK.jar -T HaplotypeCaller \
+gatk -T HaplotypeCaller \
 -R ~/Research/GCA_000182925.2_NC12_genomic_GFFtoGTFconversion.gtf \
 -I ${BAMDIR}/6147_136-11_ChIP_WT_input.bam \
 -O ${OUTDIR}/WT.vcf.gz\
