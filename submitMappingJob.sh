1e8b214d1af23ba506a326a316d42f382d90c7a9
eddietorres24 <eddietorres136@gmail.com>
2025-01-21 22:33:55 -0500

    145

--- submitMappingJob.sh
+++ submitMappingJob.sh
@@ -16,8 +16,8 @@ fi
 
 #iterates through list of accessions and passes to mapping script
 
-fastqPath="/scratch/evt82290/FastqFiles/2024_Run144_ET"
-outdir="/scratch/evt82290/MappingOutputs/Run144"
+fastqPath="/scratch/evt82290/FastqFiles/2024_Run145_ET"
+outdir="/scratch/evt82290/MappingOutputs/Run145"
 
 mkdir ${outdir}
 mkdir ${outdir}/logs
