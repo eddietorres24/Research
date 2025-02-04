51f3521ec730241695783f9578ec29a2fae8eb3b
eddietorres24 <eddietorres136@gmail.com>
2025-02-04 10:00:22 -0500

    Update GetMetadataWith_ffq.sh

--- GetMetadataWith_ffq.sh
+++ GetMetadataWith_ffq.sh
@@ -7,8 +7,8 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=50gb
 #SBATCH --time=144:00:00
-#SBATCH --output=./fetchfastq.%j.out
-#SBATCH --error=./fetchfastq.%j.err
+#SBATCH --output=../fetchfastq.%j.out
+#SBATCH --error=../fetchfastq.%j.err
 
 cd $SLURM_SUBMIT_DIR
 
@@ -20,15 +20,15 @@ accession="/home/evt82290/Research/accession_list_ET.txt"
 module load ffq/0.3.0-foss-2022a
 
 #make a directory to store fastq metadata [.JSON] format and files.
-mkdir "scratch/evt82290/downSRA/FastqFiles"
+mkdir "/scratch/evt82290/downSRA/FastqFiles"
 
 #For each line of the accession file, make a directory and fetch fastq metadata using ffq
 
 while read -r line
 do
 #make a new directory for each accession
-  mkdir "scratch/evt82290/downSRA/${line}"
+  mkdir "/scratch/evt82290/downSRA/${line}"
 #download metadata and store in a .JSON file
-  ffq -o scratch/evt82290/downSRA/${line}/${line}.json --ftp "$line"
+  ffq -o "/scratch/evt82290/downSRA/${line}/${line}.json --ftp "$line""
 
 done <"${accession}"
