fd3598f4c6d90a4735e7882b378c191d0201a878
eddietorres24 <eddietorres136@gmail.com>
2025-04-27 18:24:51 -0400

    nuc

--- MapData.sh
+++ MapData.sh
@@ -15,9 +15,9 @@ source config.txt
 
 ###ADD a source file with path to FastqFiles
 #variables imported from submission script
-fastqPath="/scratch/evt82290/FastqFiles/misc_data"
+fastqPath="/scratch/evt82290/FastqFiles/2025_Run148_ET"
 accession="148-60_ChIP_LGVI_3_H3K27me3_Rep1_S60"
-outdir="/scratch/evt82290/MappingOutputs/misc_data"
+outdir="/scratch/evt82290/MappingOutputs/Run148"
 
 # #if output directory doesn't exist, create it
 if [ ! -d $outdir ]
