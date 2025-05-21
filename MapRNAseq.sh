d2427f7ae987ee5dbd239adb917b45c550a2c013
eddietorres24 <eddietorres136@gmail.com>
2025-05-21 17:07:27 -0400

    Update MapRNAseq.sh

--- MapRNAseq.sh
+++ MapRNAseq.sh
@@ -34,9 +34,9 @@ fi
 ####################################################
 
 #input file variables
-  read1=${fastqPath}/${accession}/${accession}_1.fastq.gz
-  read2=${fastqPath}/${accession}/${accession}_2.fastq.gz
-  unpaired=${fastqPath}/${accession}/${accession}.fastq.gz
+  read1=${fastqPath}/${accession}_1.fastq.gz
+  read2=${fastqPath}/${accession}_2.fastq.gz
+  # unpaired=${fastqPath}/${accession}/${accession}.fastq.gz
 
 ###################################
 
