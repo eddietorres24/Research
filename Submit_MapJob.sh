563d7b49b63ab2d70a02acc81e220fedb9cb60dc
eddietorres24 <eddietorres136@gmail.com>
2025-08-29 11:09:09 -0400

    Update Submit_MapJob.sh

--- Submit_MapJob.sh
+++ Submit_MapJob.sh
@@ -29,10 +29,11 @@ fi
 # fastqPath="/scratch/evt82290/FastqFiles/2025_Run149_ET/RNA"
 # outdir="/scratch/evt82290/MappingOutputs/Run149/RNA"
 
-fastqPath="/scratch/evt82290/SRA/FastqFiles /scratch/evt82290/FastqFiles/2025_Run149_ET/RNA /scratch/evt82290/FastqFiles/2025_Run150_ET"
+fastqPath="/scratch/evt82290/SRA/FastqFiles /scratch/evt82290/FastqFiles/2025_Run149_ET/RNA"
 # fastqPath="/scratch/evt82290/FastqFiles/misc_RNA"
 # outdir="/scratch/evt82290/RNAseq/cac_aberrant_transcripts"
 outdir="/scratch/evt82290/MappingOutputs/RNAseq_split"
+# scratch/evt82290/FastqFiles/2025_Run150_ET
 
 mkdir ${outdir}
 mkdir ${outdir}/logs
