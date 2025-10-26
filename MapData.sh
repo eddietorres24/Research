4fd2964b3bed0326d9e72e8fd2132a792d539c39
eddietorres24 <eddietorres136@gmail.com>
2025-10-26 19:58:33 -0400

    146

--- MapData.sh
+++ MapData.sh
@@ -15,9 +15,9 @@ source config.txt
 
 ###ADD a source file with path to FastqFiles
 #variables imported from submission script
-fastqPath="/scratch/evt82290/FastqFiles/misc_data"
+fastqPath="/scratch/evt82290/FastqFiles/Run146"
 # accession="eed_comp_Courtney_2020"
-outdir="/scratch/evt82290/MappingOutputs/misc_data"
+outdir="/scratch/evt82290/MappingOutputs/Run146"
 
 # #if output directory doesn't exist, create it
 if [ ! -d $outdir ]
